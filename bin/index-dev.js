dc6c0af65653b849da31814895ab4026bc56d072
Bretislav Proft <bproft@gmail.com>
2023-05-05 14:36:13 +0200

    Remove wrong path in dev mode

--- bin/index-dev.js
+++ bin/index-dev.js
@@ -2,7 +2,7 @@
 const path = require("path");
 
 require("ts-node").register({
-  project: path.join(__dirname, "..", "scripts", "tsconfig.json"),
+  project: path.join(__dirname, "..", "tsconfig.json"),
   dir: path.join(__dirname, ".."),
 });
-require("../scripts/index.ts");
+require("../index.ts");
